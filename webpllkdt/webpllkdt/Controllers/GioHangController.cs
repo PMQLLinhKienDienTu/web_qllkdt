7aac801714d57f15cfc4e3419476e150bdba9a72
Nguyen Ngoc Thien <kinokute12@gmail.com>
2023-09-30 14:54:39 +0700

    big ud 30/09

---
+++ webpllkdt/webpllkdt/Controllers/GioHangController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webpllkdt.Models;
+
+namespace webpllkdt.Controllers
+{
+    public class GioHangController : Controller
+    {
+        ShopDBContext db = new ShopDBContext();
+        private const string CartSession = "CartSession";
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            var cart = Session[CartSession];
+            var list = new List<CartItem>();
+            if (cart != null)
+            {
+                list = (List<CartItem>)cart;
+            }
+            return View(list);
+        }
+        public ActionResult DeleteAll()
+        {
+            Session[CartSession] = null;
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            var sessionCart = (List<CartItem>)Session[CartSession];
+            sessionCart.RemoveAll(x => x.SanPhams.MaSP == id);
+            Session[CartSession] = sessionCart;
+            return RedirectToAction("index");
+        }
+        public ActionResult Update(int id, int quantity)
+        {
+            SanPham product = db.SanPhams.FirstOrDefault(c => c.MaSP == id);
+            var cart = Session[CartSession];
+            if (cart != null)
+            {
+                var list = (List<CartItem>)cart;
+                if (list.Exists(x => x.SanPhams.MaSP == id))
+                {
+
+                    foreach (var item in list)
+                    {
+                        if (item.SanPhams.MaSP == id)
+                        {
+                            item.Quantity = quantity;
+                        }
+                    }
+                }
+                //else
+                //{
+                //    //tạo mới đối tượng cart item
+                //    var item = new CartItem();
+                //    item.SanPhams = product;
+                //    item.Quantity = quantity;
+                //    list.Add(item);
+                //}
+                ////Gán vào session
+                //Session[CartSession] = list;
+            }
+            //else
+            //{
+            //    //tạo mới đối tượng cart item
+            //    var item = new CartItem();
+            //    item.SanPhams = product;
+            //    item.Quantity = quantity;
+            //    var list = new List<CartItem>();
+            //    list.Add(item);
+            //    //Gán vào session
+            //    Session[CartSession] = list;
+            //}
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult AddItem(int productId, int quantity)
+        {
+
+            SanPham product = db.SanPhams.FirstOrDefault(c => c.MaSP == productId);
+            var cart = Session[CartSession];
+            if (cart != null)
+            {
+                var list = (List<CartItem>)cart;
+                if (list.Exists(x => x.SanPhams.MaSP == productId))
+                {
+
+                    foreach (var item in list)
+                    {
+                        if (item.SanPhams.MaSP == productId)
+                        {
+                            item.Quantity += quantity;
+                        }
+                    }
+                }
+                else
+                {
+                    //tạo mới đối tượng cart item
+                    var item = new CartItem();
+                    item.SanPhams = product;
+                    item.Quantity = quantity;
+                    list.Add(item);
+                }
+                //Gán vào session
+                Session[CartSession] = list;
+            }
+            else
+            {
+                //tạo mới đối tượng cart item
+                var item = new CartItem();
+                item.SanPhams = product;
+                item.Quantity = quantity;
+                var list = new List<CartItem>();
+                list.Add(item);
+                //Gán vào session
+                Session[CartSession] = list;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- webpllkdt/webpllkdt/Controllers/HomeController.cs
+++ webpllkdt/webpllkdt/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using webpllkdt.Models;
@@ -14,7 +17,25 @@ namespace webpllkdt.Controllers
         public ActionResult Index()
         {
             var sp = db.SanPhams.ToList();
+            foreach (var item in sp)
+            {
+                if (item.HinhAnh != null && item.HinhAnh.Length > 0)
+                {
+                    string imagePath = Server.MapPath("~/img/" + item.TenSP + ".png");
+
+                    // Kiểm tra xem tệp hình ảnh đã tồn tại hay chưa
+                    if (!System.IO.File.Exists(imagePath))
+                    {
+                        // Tạo MemoryStream từ dữ liệu hình ảnh
+                        MemoryStream memoryStream = new MemoryStream(item.HinhAnh);
+
+                        // Lưu dữ liệu từ MemoryStream vào tệp hình ảnh
+                        System.IO.File.WriteAllBytes(imagePath, memoryStream.ToArray());
+                    }
+                }
+            }
             return View(sp);
         }
+
     }
 }
